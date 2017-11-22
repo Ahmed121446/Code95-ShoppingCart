53ee792a4ac1dce1923e77417351d62260c9fa7b
Apple <apple@Apples-MacBook-Pro-2.local>
2017-11-22 16:39:08 +0200

    Adding Artisan Command For FakeUser v:1.2.2

---
+++ app/Console/Commands/AddFakeUsers.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+
+
+class AddFakeUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'AddFake:Add-Fake-Users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'To add fake users with faker';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    { 
+        $FakeUsersNumber_string = $this->ask('How many fake user you need to insert in database');
+        $FakeUsersNumber = (int)$FakeUsersNumber_string; 
+        if (is_integer($FakeUsersNumber)  ) {
+            $confirmation = $this->confirm('Do you agree to insert '.$FakeUsersNumber.' fake user in user table');
+
+            if ($confirmation) {
+                
+                $choice = $this->choice('What language do you want user to be in ',['ar_SA','zh_TW','en_US']);
+
+                $faker = Faker::create($choice);
+                for ($i=1; $i <= $FakeUsersNumber; $i++) { 
+                    \DB::table('users')->insert([
+                        'name' => $faker->name,
+                        'email' => $faker->email, 
+                        'password' => bcrypt('secret'),
+                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    ]);
+                }
+                 
+            }
+        }else{
+            $this->error('please enter integer number');
+        }
+       
+
+       
+        // $number = $this->argument('number');
+        // $this->info($number);
+        // $this->info('\ndone');
+    }
+}
