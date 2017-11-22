f9554b3552af7d7ecb709fcc6a76e5285fe6a908
Apple <apple@Apples-MacBook-Pro-2.local>
2017-11-22 14:04:42 +0200

    with faker seeder v:1.2.1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //$this->call(InsertUsersSeeder::class);
+        $this->call(InsertItemsSeeder::class);
+         
     }
 }
---
+++ database/seeds/InsertUsersSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+
+class InsertUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $Languages = [
+            'ar_SA','uk_UA','zh_TW','ko_KR','en_US'
+        ];
+    	
+
+    	for ($i=1; $i <= 50; $i++) { 
+
+            if ($i <= 10 ) {
+                $faker = Faker::create($Languages[0]);
+                 DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+            }else if ($i <= 20 && $i > 10) {
+               $faker = Faker::create($Languages[1]);
+                DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+            }else if ($i <= 30 && $i > 20) {
+                $faker = Faker::create($Languages[2]);
+                 DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+            }else if ($i <= 40 && $i > 30) {
+                $faker = Faker::create($Languages[3]);
+                 DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+            }else{
+                $faker = Faker::create($Languages[4]);
+                 DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+            }
+
+    		
+    	}
+    }
+}
